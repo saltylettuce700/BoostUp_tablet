91e55ceca0f7549d22e1a1c376696a29fd9f3663
saltylettuce700 <dana.moravi@gmail.com>
2025-04-19 18:50:09 -0600

    menu linkeado signin_dueno_Activity signin_tech_Activity

--- app/src/main/java/com/example/boostup_tablet/Activity/signin_tech_Activity.java
+++ app/src/main/java/com/example/boostup_tablet/Activity/signin_tech_Activity.java
@@ -2,6 +2,8 @@ package com.example.boostup_tablet.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,9 +18,14 @@ import com.example.boostup_tablet.R;
 
 public class signin_tech_Activity extends AppCompatActivity {
 
+    ImageView btn_options;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signin_tech);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -27,9 +34,42 @@ public class signin_tech_Activity extends AppCompatActivity {
             return insets;
         });
 
+        btn_options = findViewById(R.id.btn_options);
+
         findViewById(R.id.btn_next).setOnClickListener(v -> {
             startActivity(new Intent(this, home_tech_activity.class));
             Toast.makeText(this, "IR A PERFIL", Toast.LENGTH_SHORT).show();
+        });
+
+        btn_options.setOnClickListener(v -> {
+            // Muestra el Toast de depuración
+            Toast.makeText(signin_tech_Activity.this, "Botón de opciones clickeado", Toast.LENGTH_SHORT).show();
+
+            // Crear un PopupMenu
+            PopupMenu popupMenu = new PopupMenu(signin_tech_Activity.this, btn_options);
+
+            // Inflar el menú
+            getMenuInflater().inflate(R.menu.options, popupMenu.getMenu());
+
+            // Manejar las opciones seleccionadas en el menú
+            popupMenu.setOnMenuItemClickListener(item -> {
+                if (item.getItemId() == R.id.option1) {
+                    // Redirigir a la actividad de Dueño
+                    Intent intent = new Intent(signin_tech_Activity.this, signin_dueno_Activity.class);
+                    startActivity(intent);
+                    return true;
+                } else if (item.getItemId() == R.id.option2) {
+                    // Redirigir a la actividad de Técnico
+                    Intent intent = new Intent(signin_tech_Activity.this, signin_tech_Activity.class);
+                    //startActivity(intent);
+                    return true;
+                }
+                return false;
+            });
+
+
+            // Mostrar el PopupMenu
+            popupMenu.show();
         });
     }
 }
