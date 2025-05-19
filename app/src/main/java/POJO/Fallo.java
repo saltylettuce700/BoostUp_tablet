57ff669b2ed2791420c96cac34ba7b6e1459410d
DafneEly <panchiloholly@gmail.com>
2025-05-18 22:14:15 -0600

    Funciones y logica para historial de fallos
    Se crearon las funciones para obtener el historia y llenar el recyclerview con el historial de fallos de maquina, ademas s eimplementeo el ver detalles de cada fallo

--- app/src/main/java/POJO/Fallo.java
+++ app/src/main/java/POJO/Fallo.java
@@ -1,17 +1,22 @@
 package POJO;
 
-public class Fallo {
+import java.io.Serializable;
 
+public class Fallo implements Serializable {
+
+    private int id;
     private String titulo;
     private String fecha;
     private String hora;
     private String descripcion;
+    private String tipoFalloDescripcion;
 
-    public Fallo(String titulo, String fecha, String hora) {
+    public Fallo(String titulo, String fecha, String hora, String descripcion, String tipoFalloDescripcion) {
         this.titulo = titulo;
         this.fecha = fecha;
         this.hora = hora;
         this.descripcion = descripcion;
+        this.tipoFalloDescripcion = tipoFalloDescripcion;
     }
 
     public void setTitulo(String titulo) {
@@ -30,6 +35,21 @@ public class Fallo {
         this.descripcion = descripcion;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipoFalloDescripcion() {
+        return tipoFalloDescripcion;
+    }
+
+    public void setTipoFalloDescripcion(String tipoFalloDescripcion) {
+        this.tipoFalloDescripcion = tipoFalloDescripcion;
+    }
 
     public String getTitulo() { return titulo; }
     public String getFecha() { return fecha; }
