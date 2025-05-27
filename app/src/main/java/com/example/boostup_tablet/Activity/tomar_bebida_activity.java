04a0c63a983c35641bd9a5847b40a14ca1c5a999
DafneEly <panchiloholly@gmail.com>
2025-05-27 04:26:11 -0600

    Funcion de mantenimiento al iniciar la app y handler al final Se agrego la funcion para que si al iniciar la app detecta humedad alta se vaya a mantenimiento y de ahi no pueda salir hasta solucionarlo, ademas se manda a la bd
    Se agrego un Handler para que al terminar la bebida vuelva a la pantalla principal y cierre todas las demas

--- app/src/main/java/com/example/boostup_tablet/Activity/tomar_bebida_activity.java
+++ app/src/main/java/com/example/boostup_tablet/Activity/tomar_bebida_activity.java
@@ -1,6 +1,8 @@
 package com.example.boostup_tablet.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.boostup_tablet.R;
+
+import android.os.Handler;
 
 public class tomar_bebida_activity extends AppCompatActivity {
 
@@ -22,5 +26,12 @@ public class tomar_bebida_activity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(tomar_bebida_activity.this, Idle_Activity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+
+        }, 5000);
     }
 }
